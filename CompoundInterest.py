7ba372d7ffead21c49ec25b8557332bc85d9e882
Itz OP <106766934+ItzOP69@users.noreply.github.com>
2022-06-07 22:14:15 +0530

    Add files via upload

---
+++ CompoundInterest.py
@@ -0,0 +1,6 @@
+while True:
+    p = float(input("Enter the principal amount : "))
+    t = float(input("Enter the number of years : "))
+    r = float(input("Enter the rate of interest : "))
+    ci = p * (pow((1 + r / 100), t))
+    print("Compound interest : ", int(ci))
